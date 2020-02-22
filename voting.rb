232f926e375518976d2a3642f0a7c1d68c7bdaad
Isikyus <isikyus@users.noreply.github.com>
2020-02-22 15:33:17 +1100

    Add a voting method based on known wolf status

--- voting.rb
+++ voting.rb
@@ -25,8 +25,38 @@ module Voting
     end
   end
 
+  class ByWolfStatus
+    def initialize(play_for_wolves, fallback)
+      @play_for_wolves = play_for_wolves
+      @fallback = fallback
+    end
+
+    def call(nominee, identified)
+      if identified
+        # Vote to kill anyone not on our side
+        nominee.werewolf? != @play_for_wolves
+      else
+        @fallback.call(nominee, identified)
+      end
+    end
+
+    def name
+      our_name = if @play_for_wolves
+        'against_villagers'
+      else
+        'against_wolves'
+      end
+
+      "#{our_name}(#{@fallback.name})"
+    end
+  end
+
   ALL = [
-    Yes,
-    Random
-  ].map(&:new).freeze
+    Yes.new,
+    Random.new,
+    ByWolfStatus.new(false, Yes.new),
+    ByWolfStatus.new(false, Random.new),
+    ByWolfStatus.new(true, Yes.new),
+    ByWolfStatus.new(true, Random.new)
+  ].freeze
 end
